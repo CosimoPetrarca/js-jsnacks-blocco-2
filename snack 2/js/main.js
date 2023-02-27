94ecdb54fb0d76509e8252fc754b2d36c36063d7
P4anix <pancok_1@yahoo.it>
2023-02-27 11:55:58 +0100

    snack2

--- snack 2/js/main.js
+++ snack 2/js/main.js
@@ -1,1 +1,16 @@
 'Use strict'
+
+/* Chiedi all'utente di inserire un numero */
+let numero = prompt("Inserisci un numero:");
+
+/* Converto il numero da stringa a numero intero */
+numero = Number(numero);
+
+/* Controlla se il numero è pari o dispari */
+if (numero % 2 === 0) {
+ 
+  console.log(numero);
+} else {
+  
+  console.log(numero + 1);
+}
